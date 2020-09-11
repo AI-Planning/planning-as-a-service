ccf756e7ae68b637a05d60f6aec2b1fd19826f59
Christian Muise <christian.muise@gmail.com>
2020-09-10 21:42:38 -0400

    Create unique temporary directories, and attempt to call a local planner

--- server/celery-queue/tasks.py
+++ server/celery-queue/tasks.py
@@ -2,6 +2,7 @@ import os
 import time
 import tempfile
 import requests
+import subprocess
 
 from celery import Celery
 
@@ -14,34 +15,34 @@ celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND
 def download_file( url: str, dst: str):
     r = requests.get(url)
     with open(dst, 'wb') as f:
-        f.write(r.content)    
+        f.write(r.content)
 
 @celery.task(name='tasks.solve')
 def solve(domain_url: str, problem_url: str, solver: str) -> str:
-    
-    tmpfolder = tempfile.gettempdir()
-    
+
+    tmpfolder = tempfile.mkdtemp()
+
     if WEB_DOCKER_URL != None:
         domain_url = domain_url.replace("localhost", WEB_DOCKER_URL)
         problem_url = problem_url.replace("localhost", WEB_DOCKER_URL)
 
     domain_file = f'{tmpfolder}/{os.path.basename(domain_url)}'
-    download_file(domain_url, domain_file)    
-    
+    download_file(domain_url, domain_file)
+
     problem_file = f'{tmpfolder}/{os.path.basename(problem_url)}'
     download_file(problem_url, problem_file)
-    
-    #TODO: Run planner using plan-utils
 
+    res = subprocess.run(f'{solver} {domain_file} {problem_file}',
+                            stdout=subprocess.PIPE, stderr=subprocess.PIPE,
+                            executable='/bin/bash', encoding='utf-8',
+                            shell=True, cwd=tmpfolder)
 
     # remove the tmp/fies once we finish
     os.remove( domain_file )
     os.remove( problem_file )
+    os.rmdir(tmpfolder)
 
-    return f"no solution found using {domain_file} and {problem_file} using {solver}"
-
-
-    
+    return {'stdout': res.stdout, 'stderr': res.stderr}
 
 @celery.task(name='tasks.add')
 def add(x: int, y: int) -> int:
