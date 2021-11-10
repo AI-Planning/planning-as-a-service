82c65284f63e09028f7c4721e7dad70b140f892b
YIDING-CODER <yid3@student.unimelb.edu.au>
2021-11-10 16:11:35 +1100

    Commands to update planutils

---
+++ testing/updateplautils.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+# This is a comment!
+
+git clone https://github.com/AI-Planning/planutils.git
+cd planutils
+git checkout manifest-new-version
+pip uninstall planutils
+python3 setup.py install --old-and-unmanageable
