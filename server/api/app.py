b9d716b71f6ae5d81091bc33bb9a65775170a6a2
YIDING-CODER <yid3@student.unimelb.edu.au>
2022-03-21 00:00:26 +1100

    Limiter Logic Update
    * Removed the default Limiter for all the API
    * Improve the "As much as you want" protocal for Package API.
    * Set up "lama-first" for the solver API

--- server/api/app.py
+++ server/api/app.py
@@ -30,18 +30,10 @@ app = Flask(__name__)
 # allow CORS for all domains on all routes
 CORS(app)
 
-# Limiter for DDOS attach
-
-limiter = Limiter(
-    app,
-    key_func=get_remote_address,
-    default_limits=["1/10second"]
-)
 
 
-# For API limit checking
-block_dict={}
-recent_ip=[]
+
+
 cpuCount=os.cpu_count()
 
 # Load config.py info
@@ -49,6 +41,10 @@ app.config.from_object("config")
 
 # Secret key for flashing messages back
 app.secret_key = app.config['SECRET_KEY']
+
+# For API limit checking
+block_dict={}
+LIMITER_SECONDS= app.config['LIMITER_SECONDS']
 
 # Flask-Upload
 PDDL = ('pddl',)
@@ -60,47 +56,45 @@ app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 # Solver API
 @app.route('/solver/', methods=['GET', 'POST'])
-@limiter.limit("1/10second", error_message="Sorry, we're busy. Please try again after 10 seconds.")
 def index():
+    default_package="lama-first"
     if request.method == 'GET':
-        return render_template('index.html')
+        # This is where we will send the user to the API documentation
+        if default_package in PACKAGES:
+            return jsonify(PACKAGES[default_package])
+        else:
+            return jsonify({"Error":"{} is not installed".format(default_package)})
 
     elif request.method == 'POST':
+        if check_lock():
+            if check_for_throttle(request.remote_addr):
+                abort(429, description="Sorry, we're busy. Please try again after {} seconds.".format(LIMITER_SECONDS))
 
-        # Get files
-        form_domain = request.files["domain"]
-        form_problem = request.files["problem"]
+        # Called route with a package that isn't in Planutils
+        if default_package not in PACKAGES:
+            return jsonify({"Error":"{} is not installed".format(default_package)})
         
-        # Check that files were provided
-        if form_problem.filename == '' or form_domain.filename == '':
-            flash('Domain or Problem file is missing')
-            return redirect(request.url)
-
-        # Save file with Flask-upload
-        try:
-            filename_domain = pddl_files.save(form_domain)
-            filename_problem = pddl_files.save(form_problem)
-        except UploadNotAllowed:
-            flash("The upload was not allowed")
-            return redirect(request.url)
-
-        # Files URL to send celery
-        domain_url = pddl_files.url(filename_domain)
-        problem_url = pddl_files.url(filename_problem)
-
-        # Test to call celery with a couple of solvers
-        solvers = {"lama-first"}
-        for solver in solvers:
-            task = celery.send_task('tasks.solve', args=[domain_url, problem_url, solver], kwargs={})
-            flash( Markup(f"Solving domain <a href='{domain_url}'> uploaded_domain </a> and <a href='{problem_url}'> uploaded_problem </a>: Task ID: {task.id} - <a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"))
-        # remove the tmp/files
-        # This may fail if celery tasks have not finished. May happen while debugging, or in deployed version.
+        # Grabs the request data (JSON)
+        request_data = request.get_json()
+        # Contains manifest information
+        package_manifest = PACKAGES[default_package]['endpoint']['services']["solve"]
         
-        return redirect(url_for('index'))
+        # Get all necessary arguments for the service from request_data
+        arguments = get_arguments(request_data, package_manifest)
+        if 'Error' in arguments:
+            return jsonify(arguments)
+            
+        call = package_manifest['call']
+        output_file = package_manifest['return']
+        # Send task
+        task = celery.send_task('tasks.run.package', args=[default_package, arguments, call, output_file], kwargs={})
+
+        # keep the IP and datetime of the tasks
+        block_dict[request.remote_addr]=datetime.now()
+        return jsonify({"result":str(url_for('check_task', task_id=task.id, external=True))})
 
 # Main execution route for running planutils packages
 @app.route('/package/<package>/<service>', methods=['GET', 'POST'])
-@limiter.exempt
 def runPackage(package, service):
     # Get request
     if request.method == 'GET':
@@ -112,10 +106,9 @@ def runPackage(package, service):
     
     # Post request
     elif request.method == 'POST':
-        if check_for_throttle(request.remote_addr):
-            abort(429, description="Sorry, we're busy. Please try again after 20 seconds.")
-        elif check_lock():
-            server_in_contention()
+        if check_lock():
+            if check_for_throttle(request.remote_addr):
+                abort(429, description="Sorry, we're busy. Please try again after {} seconds.".format(LIMITER_SECONDS))
 
         # Called route with a package that isn't in Planutils
         if package not in PACKAGES:
@@ -143,17 +136,15 @@ def runPackage(package, service):
         task = celery.send_task('tasks.run.package', args=[package, arguments, call, output_file], kwargs={})
 
         # keep the IP and datetime of the tasks
-        recent_ip.append(request.remote_addr)
+        block_dict[request.remote_addr]=datetime.now()
         return jsonify({"result":str(url_for('check_task', task_id=task.id, external=True))})
 
 
 
 # Redirects user to documentation for the package
 @app.route('/package')
-@limiter.limit("1/10second", error_message="Sorry, we're busy. Please try again after 10 seconds.")
 def get_available_package():
     all_packages=copy.deepcopy(PACKAGES)
-
     installed_package=settings.load()['installed']
     for package in all_packages:
         all_packages[package]["package_name"]=package
@@ -165,12 +156,10 @@ def get_available_package():
 
 # Redirects user to documentation for the package
 @app.route('/package/<package>')
-@limiter.limit("1/10second", error_message="Sorry, we're busy. Please try again after 10 seconds.")
 def get_request_for_package(package):
     return redirect(url_for('get_documentation', package=package))
 
 @app.route('/docs/<package>')
-@limiter.limit("1/10second", error_message="Sorry, we're busy. Please try again after 10 seconds.")
 def get_documentation(package):
     if package in PACKAGES:
         package_data = json.dumps(PACKAGES[package], sort_keys = True, indent = 4, separators = (',', ': '))
@@ -179,7 +168,6 @@ def get_documentation(package):
         return render_template('documentation.html', package_information='No package with that name.')
     
 @app.route('/docs/<package>/<service>')
-@limiter.limit("1/10second", error_message="Sorry, we're busy. Please try again after 10 seconds.")
 def get_documentation_service(package, service):
     if package in PACKAGES:
         if service in PACKAGES[package]['endpoint']['services']:
@@ -193,7 +181,6 @@ def get_documentation_service(package, service):
 
 # @limiter.limit("1/10second", error_message="Sorry, we're busy. Please try again after 10 seconds.")
 @app.route('/check/<string:task_id>', methods=['GET', 'POST'])
-@limiter.exempt
 def check_task(task_id: str) -> str:
     res = celery.AsyncResult(task_id)
     if res.state == states.PENDING:
@@ -242,7 +229,7 @@ def check_for_throttle(ip_address):
         return False
     last_request_time=block_dict[ip_address]
     seconds_delta=(datetime.now()-last_request_time).total_seconds()
-    return seconds_delta<20
+    return seconds_delta<LIMITER_SECONDS
 
 
 # The maximum number of threads for a worker is same as the CPU counts.
@@ -258,10 +245,7 @@ def check_lock():
         return True
     else:
         return False
-def server_in_contention():
-    for ip in recent_ip:
-        block_dict[ip]=datetime.now()
-    recent_ip.clear()
+        
     
 if __name__ == "__main__":
     app.run("0.0.0.0", port=5001, debug=True)
