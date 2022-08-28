f63956923a2a441822ee7443488b67cc3bbc5317
YIDING-CODER <yid3@student.unimelb.edu.au>
2022-08-28 18:24:29 +1000

    Change Redis Backend to Mysql

--- server/api/config.py
+++ server/api/config.py
@@ -1,5 +1,5 @@
 SECRET_KEY = 'very_very_secure_and_secret'
 CELERY_BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'db+mysql://user:password@mysql:3306/db'
 UPLOAD_FOLDER = 'tmp'
 LIMITER_SECONDS=20
