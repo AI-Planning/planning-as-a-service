f5bdcf868bcaaf01b1fda85fcaf1be71d30dcc41
Christian Muise <christian.muise@gmail.com>
2024-02-14 00:18:03 -0500

    Wrap planner arguments in quotes.

--- server/celery-queue/tasks.py
+++ server/celery-queue/tasks.py
@@ -129,7 +129,9 @@ def run_package(self, package: str, arguments:dict, call:str, output_file:dict,*
                 call = call.replace("{%s}" % k, str(v['value']))
 
         # add the timeout to the call
-        call = f"timeout {TIME_LIMIT} planutils run {call}"
+        planner = call.split(' ')[0]
+        args = ' '.join(call.split(' ')[1:])
+        call = f"timeout {TIME_LIMIT} planutils run {planner} \"{args}\""
         res = subprocess.run(call, stdout=subprocess.PIPE, stderr=subprocess.PIPE,
                             executable='/bin/bash', encoding='utf-8',
                             shell=True, cwd=tmpfolder)
